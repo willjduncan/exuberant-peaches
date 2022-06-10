5d80497520b1aa0ab711c40a250e884ac5b9697d
willduncan <willdunc12@gmail.com>
2022-06-10 17:41:52 -0400

    add resume and style resume page

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import coverImage from '../../../assets/cover/cover-image.png';
-
+import React from "react";
+import coverImage from "../../../assets/cover/cover-image.png";
+import resume from "../../../assets/documents/resume.pdf";
 
 function Resume() {
   return (
     <section className="my-5">
-    <section id="resume" class="flex-row">
-    <h2 className="contact-style section-title  primary-border">Resume</h2>
-    <div>
-        <h3>Take a look at my resume <a href="../Professional-Portfolio/assets/documents/resume.pdf" target="-blank">here.</a></h3>
-    </div>
-</section>
+      <section id="resume" class="flex-row">
+        <h2 className="resume-title">Skills</h2>
+            <h3 className="resume-link">
+              Download my resume{" "}
+              <a href={resume} target="_blank" >
+                here.
+              </a>
+            </h3>
+        <div>
+          <ul className="skill-lists">
+            {" "}
+            <p className="skill-title">Front-End Proficiencies</p>
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>React.js</li>
+            <li>Handlebars</li>
+            <li>JQuery</li>
+            <li>Jest</li>
+          </ul>
+          <ul className="skill-lists">
+            <p className="skill-title">Back-End Proficiencies</p>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>SQL</li>
+            <li>Sequelize</li>
+            <li>NoSQL</li>
+            <li>MongoDB</li>
+            <li>Mongoose</li>
+            <li>IndexedDB</li>
+            <li>Insomnia</li>
+          </ul>
+        </div>
+      </section>
     </section>
   );
 }
 
 export default Resume;
-
-
-
-
-
-
