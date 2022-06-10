c48c07e0b798c4ef705101a85c6e26d6b308b126
willduncan <willdunc12@gmail.com>
2022-06-10 18:47:47 -0400

    style contact section

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -16,7 +16,7 @@ function ContactForm() {
   };
 
   const handleChange = (e) => {
-    if (e.target.name === 'email') {
+    if (e.target.name === 'Email') {
       const isValid = validateEmail(e.target.value);
       if (!isValid) {
         setErrorMessage('Your email is invalid.');
@@ -37,20 +37,21 @@ function ContactForm() {
   };
 
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
+    <section className='contact-bg'>
+      <div className='contact-container'>
+      <h1 className='contact-title'>Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          <label htmlFor="Name">Name:</label>
+          <input type="text" name="Name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+          <label htmlFor="Email">Email address:</label>
+          <input type="email" name="Email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          <label htmlFor="Message">Message:</label>
+          <textarea name="Message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
           <div>
@@ -59,6 +60,8 @@ function ContactForm() {
         )}
         <button data-testid="button" type="submit">Submit</button>
       </form>
+      </div>
+        
     </section>
   );
 }
